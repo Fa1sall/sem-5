dfb6a3900788e084f594ba7b3cc18c7d39fea740
Fa1sall <faisalirfan7c@gmail.com>
2024-08-04 10:46:06 +0530

    Caesar Cipher

--- network_cryptography/1_CaesarCipher/CaesarCipher.java
+++ network_cryptography/1_CaesarCipher/CaesarCipher.java
@@ -1,45 +1,60 @@
 import java.util.Scanner;
 
 public class CaesarCipher{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("---Caesar Cipher Menu:---");
-        System.out.println("1 => Encrypt Data");
-        System.out.println("2 => Decrypt Data");
-        System.out.print("Enter choice (1 or 2): ");
-        int choice = scanner.nextInt();
+        System.out.println("1 -> Encrypt Data");
+        System.out.println("2 -> Decrypt Data");
+        System.out.print("Enter choice: ");
+        int choice = Integer.valueOf(scanner.nextLine());
 
         switch(choice){
             case 1:
-            System.out.println("Enter text to Encrpt: ");
-            System.out.println("Enter shiftKey: ");
-            int shiftKey = scanner.nextInt();
-            System.out.println(EncryptText("XYZ", shiftKey));
+                System.out.print("Enter text to Encrpt/Decrypt: ");
+                String input = scanner.nextLine();
+                System.out.print("Enter shiftKey: ");
+                int shiftKey = scanner.nextInt();
+                System.out.println(EncryptText(input, shiftKey));
+                break;
+
+            case 2:
+                System.out.print("Enter text to Encrpt/Decrypt: ");
+                input = scanner.nextLine();
+                System.out.print("Enter shiftKey: ");
+                shiftKey = scanner.nextInt();
+                System.out.println(DecryptText(input, shiftKey));
+                break;
+
+            default:
+                System.out.println("Invalid Choice");
+                break;
         }
     }
 
     public static String EncryptText(String text,int shift){
         StringBuilder encryptedText = new StringBuilder();
-        char encryptChar= ' ';
-        for(char i=0;i<text.length();i++){
-            int charPosition = (int) text.charAt(i); 
-            if(!(text.charAt(i)==' ')){
-                if(charPosition+shift>26){
-                    int finalPosition = (charPosition+shift)-26;
-                    encryptChar = (char) finalPosition;
-                    encryptedText.append(encryptChar);
+        char letterCase = ' ';
+        for(int i=0;i<text.length();i++){
+            if(text.charAt(i)!=' '){
+                if(text.charAt(i)>='a' && text.charAt(i)<='z'){
+                    letterCase = 'a';
                 } else {
-                    encryptChar = (char)(text.charAt(i) + shift);
-                    encryptedText.append(encryptChar);
+                    letterCase = 'A';
                 }
-            }else{
+                int pos = text.charAt(i) - letterCase;
+                int encryptedPos = (pos + shift) % 26;
+                char encryptedChar = (char) (letterCase + encryptedPos);
+                encryptedText.append(encryptedChar);
+            } else {
                 encryptedText.append(' ');
             }
         }
-        return encryptedText.toString();
+
+        return ("Result: " + encryptedText.toString());
     }
 
     public static String DecryptText(String text,int shift){
-        return "";
+        return EncryptText(text,26-(shift%26));
     }
 }
