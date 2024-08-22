6b6c148c72bdf69428372bb2e4d68aa239fc8862
Fa1sall <faisalirfan7c@gmail.com>
2024-08-22 22:39:03 +0530

    Extended Euclidean Algorithm

---
+++ network_cryptography/4_ExtendedEuclideanAlgorithm/ExtendedEuclideanAlgorithm.java
@@ -0,0 +1,37 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class ExtendedEuclideanAlgorithm {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a: ");
+        long a = scanner.nextInt();
+        System.out.print("b: ");
+        long b = scanner.nextInt();
+        gcdExtended(a, b);
+    }
+
+    public static void gcdExtended(long a, long b)
+    {
+        long x = 0, y = 1, lastx = 1, lasty = 0, temp;
+        while (b != 0)
+        {
+            long q = a / b;
+            long r = a % b;
+
+            a = b;
+            b = r;
+
+            temp = x;
+            x = lastx - q * x;
+            lastx = temp;
+
+            temp = y;
+            y = lasty - q * y;
+            lasty = temp;
+        }
+        System.out.println("GCD "+a+" and its Roots x: "+ lastx +" y:"+ lasty);
+    }
+}
+
