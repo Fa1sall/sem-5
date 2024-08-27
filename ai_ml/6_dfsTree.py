057670ce407709920a79a4a30f1d5d4b8ee908f2
Fa1sall <faisalirfan7c@gmail.com>
2024-08-27 21:21:06 +0530

    DFS For Tree

--- ai_ml/6_dfsTree.py
+++ ai_ml/6_dfsTree.py
@@ -4,30 +4,37 @@ class Node:
 		self.right = None
 		self.val = key
 
-
 def printInorder(root):
 	if root:
+		# First recur on left child
 		printInorder(root.left)
 
+		# then print the data of node
 		print(root.val),
 
+		# now recur on right child
 		printInorder(root.right)
-
 
 def printPostorder(root):
 	if root:
+		# First recur on left child
 		printPostorder(root.left)
 
+		# the recur on right child
 		printPostorder(root.right)
 
+		# now print the data of node
 		print(root.val),
 
 def printPreorder(root):
 	if root:
+		# First print the data of node
 		print(root.val),
 
+		# Then recur on left child
 		printPreorder(root.left)
 
+		# Finally recur on right child
 		printPreorder(root.right)
 
 
@@ -36,7 +43,7 @@ root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-print("\n Preorder traversal of binary tree is")
+print ("Preorder traversal of binary tree is")
 printPreorder(root)
 
 print ("\nInorder traversal of binary tree is")
